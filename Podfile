c80602f040788c4f73bed09425593f0057aa4bf1
wangjue <wangjue@rongcloud.cn>
2020-08-21 14:50:33 +0800

    提交工程代码

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'rongcloud-ios-imdemo-userinfo&groupinfo' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for rongcloud-ios-imdemo-userinfo&groupinfo
+
+  pod 'RongCloudIM/IMKit', '~> 4.0.1.1'
+  pod 'RongCloudIM/IMLib', '~> 4.0.1.1'
+
+end
---
+++ rongcloud-ios-imdemo-userinfo&groupinfo/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  rongcloud-ios-imdemo-userinfo&groupinfo
+//
+//  Created by Jue on 2020/8/19.
+//  Copyright © 2020 RC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+}
+
+
+@end
